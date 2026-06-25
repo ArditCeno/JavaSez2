c567b3d7108b39b107b511d37d2a165b167e96ae
ArditCeno <arditceno1@gmail.com>
2026-06-25 16:13:17 +0200

    Hoqa PerseritjeArdit, Leksione, UML nga git

--- src/PerseritjeArdit/d.java
+++
@@ -1,5 +0,0 @@
-//package PerseritjeArdit;
-////Le të jetë n një numër i plotë dhe x një numër me presje notuese.
-//// Shpjegoni ndryshimin midis tyre. a. n = (int) x; dhe n = (int) Math.round(x);
-//public class d {
-//}
